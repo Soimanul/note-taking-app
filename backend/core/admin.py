e7745361bee70943c6c223d5370660a42bc1977f
Soimanul <--global>
2025-10-01 08:25:50 +0200

    add models to admin panel

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Document, GeneratedContent, Log
 
-# Register your models here.
+
+admin.site.register(Document)
+admin.site.register(GeneratedContent)
+admin.site.register(Log)
